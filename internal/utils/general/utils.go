675449c7003ece0bf56aa1e7fb70581678884477
IdzAnAG1 <Susan0N0Mik0t0@outlook.com>
2025-06-26 23:43:32 +0300

    Fix : 	1) README.md has been updated 	2) in utils.go function GetFileContent has been removed because she was dubbing ReadFromFile function

--- internal/utils/general/utils.go
+++ internal/utils/general/utils.go
@@ -6,29 +6,12 @@ import (
 	"log"
 	"os"
 	"path"
-	"path/filepath"
 )
 
 func ConvertToHash(content string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(content))
 	return hex.EncodeToString(hasher.Sum(nil))
-}
-
-func GetFileContents(pathToDir string, fileName string) (string, error) {
-	file, err := os.OpenFile(filepath.Join(pathToDir, fileName), os.O_RDONLY, os.ModePerm)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	buff := make([]byte, 1024)
-	_, err = file.Read(buff)
-
-	if err != nil {
-		return "", err
-	}
-	return string(buff), nil
 }
 
 func ReadFromFile(dir string, file os.DirEntry) string {
